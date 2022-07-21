561cb54edaa735bf3f0000732f45afe071f601b9
Bernardo <bernardo_quetglas@hotmail.com>
2022-07-21 08:49:30 -0300

    js page created

---
+++ javascipt.js
@@ -0,0 +1,8 @@
+function mostrar(){
+    swal({
+        title: "Gracias por contactarnos!",
+        text: "Te estaremos respondiendo a la brevedad!",
+        icon: "success",
+        button: false,
+        });
+}
