9e37cb99e4502b5bceab2db56426408a54127c10
Bernardo Quetglas <bernardo_quetglas@hotmail.com>
2022-07-30 13:33:35 -0300

    scroll fade in scss, js, added

--- javascipt.js
+++ javascipt.js
@@ -1,8 +1,30 @@
+
+//LOADER PAGE FUNCTION
 window.onload = function(){
-    $("#onload").fadeOut(2000);
-    $("body").removeClass("hidden");
+    $('#onload').fadeOut(1500);
+    setTimeout(function() {
+        $('#body').removeClass('hidden');
+    }, 1400);
 }
 
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+        } else {
+        reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+
+//CONTACT FORM FUNCTION
 function mostrar(){
     swal({
         title: "Gracias por contactarnos!",
